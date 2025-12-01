a8c17e9bb3675cfa4ea2fa92dddaf07036a84c93
MojtabaNabid <mojtaba.nabid@gmail.com>
2025-12-01 21:18:42 +0100

    deletAllHandler and deleteHandler function added

--- js/app.js
+++ js/app.js
@@ -3,6 +3,17 @@ const taskDate = document.getElementById("date-input"); // selecting date input
 const addButton = document.getElementById("add-button"); // selecting the Add button
 const alertMessage = document.getElementById("alert-message"); // selecting the alert message
 const deleteAll = document.getElementById("delete-all-button");
+const todosBody = document.querySelector("tbody");
+// console.log(todosBody);
+
+// the initial value of todos comes from localstorage and if it's null, empty array goes to todos
+let todos = JSON.parse(localStorage.getItem("todos")) || []; // or sign checks null value(false)
+// console.log(todos);
+
+// function for saving new tasks into localstorage
+const saveToLocalStorage = () => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}; 
 
 // create uniqe Id for every task
 const generateId = () => {
@@ -26,85 +37,30 @@ const showAlert = (message, type) => {
   }, 2000);
 };
 
-// the initial value of todos comes from localstorage and if it's null, empty array goes to todos
-const todos = JSON.parse(localStorage.getItem("todos")) || []; // or sign checks null value(false)
-// console.log(todos);
-
-// function for saving new tasks into localstorage
-const saveToLocalStorage = () => {
-  localStorage.setItem("todos", JSON.stringify(todos));
-};
-
 // create list of tasks from localstorage
-const createList = () => {
-  const tasks = JSON.parse(localStorage.getItem("taskData")); // getting back data from local storage
-  console.log("tasks inside creatList function: ", tasks);
-  //   console.log(newTask[`task${counter}`]);
-  // selecting the table node
-  const table = document.querySelector("table");
-  const tbody = table.querySelector("tbody");
-  tbody.innerHTML = "";
-
-  //   console.log(tasks);
-  for (let task in tasks) {
-    // console.log(task);
-    // adding new row to the table
-    const newRow = document.createElement("tr");
-    tbody.appendChild(newRow);
-
-    // creating Name cell
-    const newTaskName = document.createElement("td");
-    newTaskName.innerText = tasks[task].taskName; // adding the new task name into its cell
-    tbody.lastChild.appendChild(newTaskName);
-
-    // creating date cell
-    const newTaskDate = document.createElement("td");
-    newTaskDate.innerText = tasks[task].taskDate;
-    tbody.lastChild.appendChild(newTaskDate);
-
-    // creating status cell
-    const newTaskStatus = document.createElement("td");
-    newTaskStatus.innerText = tasks[task].taskStatus;
-    tbody.lastChild.appendChild(newTaskStatus);
-
-    // creating Action cell
-    const newTaskAction = document.createElement("td");
-
-    // creating Edit-Do-Delete buttons
-    const editButton = document.createElement("button");
-    editButton.innerText = "Edit";
-    const doButton = document.createElement("button");
-    doButton.innerText = "Do";
-    const deleteButton = document.createElement("button");
-    deleteButton.innerText = "Delete";
-
-    // adding classes to the edit-do-delete buttons
-    editButton.classList.add("edit");
-    doButton.classList.add("do");
-    deleteButton.classList.add("delete");
-
-    // adding buttons to their cell
-    newTaskAction.appendChild(editButton);
-    newTaskAction.appendChild(doButton);
-    newTaskAction.appendChild(deleteButton);
-
-    // adding action cell to the row
-    tbody.lastChild.appendChild(newTaskAction);
+const dispalayToDos = () => {
+  todosBody.innerHTML = "";
+  if(!todos.length)  {
+    todosBody.innerHTML = "<tr><td colspan='4'>No Task Found!</td></tr>";
+    return;
   }
 
-  // chera man in ghesmat payeen ta khat 83 ro neveshtam(!?)
-  const rows = document.querySelector("tbody").querySelectorAll("tr");
-  // console.log(rows);
-  for (let row of rows) {
-    const status = row.children[2].innerText;
-    if (status === "pending") {
-      row.dataset.status = "pending";
-      // console.log(row);
-    } else if (row.children[2].innerHTML === "completed") {
-      newRow.dataset.status = "completed";
-    }
-  }
-};
+  todos.forEach(todo => {
+    todosBody.innerHTML += `
+      <tr>
+        <td>${todo.task}</td>
+        <td>${todo.date || "No Date"}</td>
+        <td>${todo.completed ? "Completed" : "Pending"}</td>
+        <td>
+          <button>Edit</button>
+          <button>Do</button>
+          <button onclick="deleteHandler('${todo.id}')">Delete</button>
+        </td>
+      </tr>
+    `
+    // to work with action buttons we give the element onclick attribute 
+  });
+}
 
 // add new tasks 
 const addHandler = (event) => {
@@ -123,46 +79,40 @@ const addHandler = (event) => {
     saveToLocalStorage();
     taskInput.value = "";
     taskDate.value = "";
+    dispalayToDos()
     showAlert("Task added successfuly!", "success");
-    console.log(todos);
+    // console.log(todos);
   } else {
     showAlert("please enter a task!", "error");
   }
 };
 
+// delete all tasks
 const deleteAllHandler = (event) => {
-  localStorage.clear();
-  createList();
+  // localStorage.clear();  // one way is like this
+  // console.log("delte all");
+  if(todos.length) {  // second way to delete all tasks
+    todos = []
+    saveToLocalStorage();
+    dispalayToDos()
+    showAlert("All tasks deleted successfully", "success")
+  } else showAlert("No Tasks to Delet", "error")
 };
 
-const filterHandler = (event) => {
-  if (event.target.dataset.filter === "all") {
-    createList();
-  } else if (event.target.dataset.filter === "pending") {
-    console.log(true);
-    const rows = document.querySelectorAll("tr");
-    for (let row of rows) {
-      if (row.dataset.status) {
-      }
-    }
-    // const tasks = JSON.parse(localStorage.getItem("taskData"))
-    // for (let task in tasks)
-    //     if(tasks[task].taskStatus === "pending"){
-    //         console.log(true);
-    //     }
-    document.querySelectorAll("td");
-  }
-  // else {
+const deleteHandler = (id) => {
+  // console.log(id);
+  const newToDos = todos.filter(e => e.id !== id);
+  // console.log(newToDos);
+  todos = newToDos;
+  saveToLocalStorage()
+  dispalayToDos();
+}
 
-  // }
-
-  // const buttons = document.querySelectorAll("div.buttons button")
-  // console.log(object);
-};
-
-createList();
+// when window loads upload todos from local storag on the table
+window.addEventListener("load", dispalayToDos)
 
 // add event listener for Add button
 addButton.addEventListener("click", addHandler);
+
 // add event listener for delet all button
 deleteAll.addEventListener("click", deleteAllHandler);
